af91bcd724465d27c47614a74c4ee3f504964b0a
Wilson Wu <iwilsonwu@gmail.com>
2017-09-15 00:33:40 +0800

    remove block

--- src/vue-funretro/src/view/board.vue
+++ src/vue-funretro/src/view/board.vue
@@ -1,6 +1,6 @@
 <template>
     <v-layout row wrap>
-        <v-flex xs="12" sm="12" md="4" lg="4" v-for="column in columns" :key="column">
+        <v-flex v-for="column in columns" :key="column">
             {{ column }}
         </v-flex>
     </v-layout>
